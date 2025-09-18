f8476646ba5836bd1d0ed631853f6a970bdcb769
Elliot Akande <elbo.lynch@gmail.com>
2025-09-18 22:26:41 +0100

    feat: flexible suffix generation

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,83 @@ import "./styles.css";
 const generateButton = document.querySelector(".generate");
 const linkElement = document.querySelector(".link");
 
+const rand = (min: number, max: number): string => {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num.toString().padStart(max.toString().length);
+};
+
 type PatternType = "RAND_4";
 interface Pattern {
   prefix: string;
-  type: PatternType;
+  getSuffix: () => string;
 }
 const patterns: Pattern[] = [
-  { prefix: "MVI ", type: "RAND_4" },
-  { prefix: "MOV ", type: "RAND_4" },
-  { prefix: "100 ", type: "RAND_4" },
-  { prefix: "SAM ", type: "RAND_4" },
-  { prefix: "DSC ", type: "RAND_4" },
-  { prefix: "SDV ", type: "RAND_4" },
-  { prefix: "DSCF", type: "RAND_4" },
-  { prefix: "DSCN", type: "RAND_4" },
-  { prefix: "PICT", type: "RAND_4" },
-  { prefix: "MAQ0", type: "RAND_4" },
-  { prefix: "FILE", type: "RAND_4" },
-  { prefix: "GOPR", type: "RAND_4" },
-  { prefix: "GP01", type: "RAND_4" },
-  { prefix: "GX01", type: "RAND_4" },
-  { prefix: "DJI ", type: "RAND_4" },
+  {
+    prefix: "MVI ",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "MOV ",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "100 ",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "SAM ",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "DSC ",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "SDV ",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "DSCF",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "DSCN",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "PICT",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "MAQ0",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "FILE",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "GOPR",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "GP01",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "GX01",
+    getSuffix: () => rand(0, 9999),
+  },
+  {
+    prefix: "DJI ",
+    getSuffix: () => rand(0, 9999),
+  },
 ];
 
 const generateQuery = (): string => {
-  const pattern = patterns[Math.floor(Math.random() * patterns.length)];
-  const randString = Math.floor(Math.random() * 9999)
-    .toString()
-    .padStart(4, "0");
-  const query = `${pattern.prefix}${randString}`;
-
+  const randIndex = Math.floor(Math.random() * patterns.length);
+  const { prefix, getSuffix } = patterns[randIndex];
+  const query = `${prefix}${getSuffix()}`;
   return query;
 };
 
