daf611f824b5d408c842cc10d2b41aa21de6cd39
Elliot Akande <elbo.lynch@gmail.com>
2025-09-20 17:31:07 +0100

    style: move attribution below main div

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,51 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index.ts",
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset",
-        parser: { dataUrlCondition: { maxSize: 8 * 1024 } },
-      },
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+
+  return {
+    mode: isProd ? "production" : "development",
+    entry: "./src/index.ts",
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset",
+          parser: { dataUrlCondition: { maxSize: 8 * 1024 } },
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  optimization: {
-    minimizer: [`...`, new CssMinimizerPlugin()],
-  },
-  devServer: {
-    static: "./dist",
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    optimization: {
+      minimizer: [`...`, new CssMinimizerPlugin()],
+    },
+    devServer: {
+      static: "./dist",
+    },
+  };
 };
